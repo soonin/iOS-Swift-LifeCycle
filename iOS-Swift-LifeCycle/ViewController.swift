e6a24e1a19d3981953ef13b3206e7ffaa47ebcb7
Pooya Hatami <am.hatami@gmail.com>
2020-05-10 14:18:44 -0700

    Initial Commit

---
+++ iOS-Swift-LifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Swift-LifeCycle
+//
+//  Created by Pooya on 2020-05-10.
+//  Copyright © 2020 centurytrail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
